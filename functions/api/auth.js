709365ade574a54012e8c3823f259e5eea53ca0f
Aspirefyx <abhisek@aspirefyx.com>
2026-03-23 13:15:40 +0530

    Update auth.js

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -1,6 +1,10 @@
 export async function onRequest(context) {
+  const { env } = context;
+  const client_id = env.GITHUB_CLIENT_ID; // This pulls from Cloudflare Settings
+
   const url = new URL("https://github.com/login/oauth/authorize");
-  url.searchParams.set("client_id", context.env.GITHUB_CLIENT_ID);
+  url.searchParams.set("client_id", client_id);
   url.searchParams.set("scope", "repo,user");
+  
   return Response.redirect(url.href, 302);
 }
